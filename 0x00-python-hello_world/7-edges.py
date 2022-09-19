cf425c9a98f5b93c7ad12264468efe7563a6f4c1
MuneneCalvin <munenecalvn@gmail.com>
2022-09-19 07:17:05 +0000

    Hello World

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: { }".format(word_first_3))
+print(f"Last 2 letters: { }".format(word_last_2))
+print(f"Middle word: { }".format(middle_word))
---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is interpreted, interactive, object-oriented programming\
+language that combines remarkable power with every clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)

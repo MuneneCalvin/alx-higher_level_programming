c0e37933d252a58f9af4429d035e0e960d3cbab9
MuneneCalvin <munenecalvn@gmail.com>
2022-09-20 02:53:57 -0700

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for letter i range (97, 1223):
-    if chr(letter) is not  'q' and chr(letter) is not 'e'"
-        print("{}".format(letter)), end="")
+for letter in range (97, 1223):
+    if chr(letter) is not  'q' and chr(letter) is not 'e':
+        print("{}".format(letter), end="")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for numbers in range (0, 100):
+for number in range (0, 100):
     if number == 99:
         print("{}".format(number))
     else:
-        print("{.02}".format(number), end=", ")
-
+        print("{:02}".format(number), end=", ")

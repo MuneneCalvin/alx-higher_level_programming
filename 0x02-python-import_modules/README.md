a2d1de7eefadfe50c0e0cb7d8e3a13acd6d1f4f9
MuneneCalvin <munenecalvn.com>
2022-09-22 11:11:08 +0300

    Import Modules

---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,5 @@
+# Python - import & modules
+
+In this project, I learned about importing and using functions and creating
+modules in Python. I further practiced using the builtin function
+`dir()` and using command line arguments within Python programs.
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Argvs:
+        a: first integer
+        b: second interger
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My subtraction function
+
+    Argvs:
+    a: first interger
+    b: second interger
+    Returns:
+        The result value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication
+
+    Arvgs:
+    a: first interger
+    b: second interger
+    Returns:
+        The result value. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """MY subtration
+
+    Argvs:
+    a: first intergr
+    b: second interger
+    Rerurns
+        The result value. a/b
+    """
+    return int(a / b)
